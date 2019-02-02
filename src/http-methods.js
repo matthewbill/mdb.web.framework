2ae53c4180233967d4e453de81ca5ab1d8cbf0ba
matthewbill <matthewdbill@gmail.com>
2019-02-02 15:45:24 +0000

    fixed linting rules

--- src/http-methods.js
+++ src/http-methods.js
@@ -7,8 +7,11 @@
  */
 class HttpMethods {
   static get GET() { return 'GET'; }
+
   static get PUT() { return 'PUT'; }
+
   static get POST() { return 'POST'; }
+
   static get DELETE() { return 'DELETE'; }
 }
 
